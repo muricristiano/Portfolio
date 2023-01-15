393d0dc9adf1a54570ee38df718a2f12f59b8cfd
Murilo Ribeiro Cristiano <muricristiano@gmail.com>
2023-01-15 18:51:44 -0300

    Navigation menu + About me info

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,6 +10,7 @@ const Header = () => {
       <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1>Murilo Cristiano</h1>
+        <h5 className="text-light">Javascript / React / Node.js</h5>
         <h5 className="text-light">Fullstack Developer</h5>
         <CTA />
         <HeaderSocials />
