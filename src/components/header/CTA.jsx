f03391c843de966c463335ee547d4dc5f330de5e
Murilo Cristiano <murilocristiano@Muri-Macbook.station>
2023-07-04 20:43:04 +0200

    update cv file

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import './header.css'
 import CV from '../../assets/murilo-cristiano-cv.pdf'
 
+
 const CTA = () => {
   return (
     <div className='cta'>
-        <a href={CV} download className='btn'>Download CV</a>
-        <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+      <a href={CV} download className='btn'>Download CV</a> 
+      <a href="#contact" className='btn btn-primary'>Let's Talk</a>
     </div>
   )
 }
