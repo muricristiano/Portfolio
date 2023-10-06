ecf5e8d63ab93c0bdd42ab01f58e805f08a007f9
Murilo Ribeiro Cristiano <muricristiano@gmail.com>
2023-10-06 12:12:07 +0100

    cv updated / about section updated

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -45,7 +45,7 @@ const About = () => {
 
             <p><span className='bold-span'>Nature explorer, Neuroscience enthusiast & Visual arts lover. <br/> I'm a <span className='primary-color-span'>JavaScript Full Stack Developer</span>.</span></p>
             <h5>Past</h5>
-            <p>I have a background in high responsability companies who for example developed softwares for Juridic Systems. My professional experience has allowed me to develop a range of competencies, including JavaScript, TypeScript, React, Node.js, etc. (All skills are on competences sections with Linkedin's Certificate Competence Seals).</p>
+            <p>I have a background in high responsibility companies who for example developed softwares for Juridic Systems. My professional experience has allowed me to develop a range of competencies, including JavaScript, TypeScript, React, Node.js, etc. (All skills are on competences sections with Linkedin's Certificate Competence Seals).</p>
             <h5>Present</h5>
             <p>I am a motivated individual with a passion for upgrading people's lives with technology. I am constantly seeking new challenges to grow and learn, both professionally and personally.</p>
             <h5>Future</h5>
